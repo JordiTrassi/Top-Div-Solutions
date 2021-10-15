384efe485300b57b54069a1661a9fbc6f5857bed
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2021-10-15 21:40:39 +0200

    Create views
    -Create LandingPage module
    -Create ServicesPage module
    -Create PortfolioPage module
    -Create TeamPage module
    -Create BlogPage module
    -Create ArticlePage module
    -Create ContactPage module
    -Delete default views: HelloWorld.vue, About.vue, Home.vue

---
+++ frontend/src/views/ArticlePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Yo soy la Pagina de un Articulo</h2>
+    <h2>Creo que no tendré componentes</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ArticlePage",
+});
+</script>
+
+<style scoped lang="Scss">
+</style>
---
+++ frontend/src/views/LandingPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Yo soy la LandingPage</h2>
+    <h3>Uno de mis componentes es el Formulario de contacto</h3>
+    <ContactForm />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ContactForm from "../components/ContactForm.vue";
+
+export default defineComponent({
+  name: "LandingPage",
+  components: {
+    ContactForm,
+  },
+});
+</script>
+
+<style scoped lang="Scss">
+</style>
